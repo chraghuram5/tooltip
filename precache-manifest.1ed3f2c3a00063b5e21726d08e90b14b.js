cdc18408915115a580405c9fb07d4d4a8c6d4687
chraghuram5 <ch.raghuram5@gmail.com>
2020-08-09 22:13:18 +0530

    Updates

---
+++ precache-manifest.1ed3f2c3a00063b5e21726d08e90b14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630b94ace8c195ce9026ced97b0a10e8",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "a597c7b92095442e61ca",
+    "url": "/tooltip/static/css/main.e87c5a29.chunk.css"
+  },
+  {
+    "revision": "b9a6d342d061d35b79d8",
+    "url": "/tooltip/static/js/2.31aa36a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip/static/js/2.31aa36a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a597c7b92095442e61ca",
+    "url": "/tooltip/static/js/main.46275d17.chunk.js"
+  },
+  {
+    "revision": "44237ef0bd03b0a879d5",
+    "url": "/tooltip/static/js/runtime-main.7b5cc87c.js"
+  }
+]);
